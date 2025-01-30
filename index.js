51edd18a8338f9196bb2875b0a9327dec882a87b
unknown <dilorenzo@itis-molinari.eu>
2025-01-30 16:09:12 +0100

    codice pushato

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+const fs = require('fs');
+
+const mysql = require('mysql');
+
+const conf = JSON.parse(fs.readFileSync('conf.json'));
+
+const connection = mysql.createConnection(conf);
+
+
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
@@ -38,13 +47,6 @@ console.log(todos);
 
 });
 
-const server = http.createServer(app);
-
-server.listen(80, () => {
-
-  console.log("- server running");
-
-});
 
 app.put("/todo/complete", (req, res) => {
 console.log("dentro");
@@ -106,4 +108,76 @@ app.put("/todo/modify", (req, res) => {
    
       res.json({result: "Ok"});
    
+   });
+  
+   const executeQuery = (sql) => {
+      return new Promise((resolve, reject) => {      
+         connection.query(sql, function (err, result) {
+            if (err) {
+               console.error(err);
+               reject(err); 
+               return; 
+            }   
+            console.log('done');
+            resolve(result);         
+         });
+      });
+   };
+   
+const createTable = () => {
+   console.log("create table")
+   
+      return executeQuery(`
+   
+      CREATE TABLE IF NOT EXISTS todo
+   
+         ( id INT PRIMARY KEY AUTO_INCREMENT, 
+   
+            name VARCHAR(255) NOT NULL, 
+   
+            completed BOOLEAN ) 
+   
+         `);      
+   
+   }
+const insert = (todo) => {
+   
+      const template = `
+   
+      INSERT INTO todo (name, completed) VALUES ('$NAME', '$COMPLETED')
+   
+         `;
+   
+      let sql = template.replace("$NAME", todo.name);
+   
+      sql = sql.replace("$COMPLETED", todo.completed);
+   
+      return executeQuery(sql); 
+   
+   }
+const select = (todo) => {
+   
+      const sql = `
+   
+      SELECT id, name, completed FROM todo 
+   
+         `;
+   
+      return executeQuery(sql); 
+   
+   }  
+const server = http.createServer(app);
+
+server.listen(80, () => {
+
+  console.log("- server running");
+
+});
+createTable()
+   .then(() => {
+     return insert({ name: "test " + new Date().getTime(), completed: false });
+   })
+   .then(() => select().then("database: "+console.log))
+   .catch((err) => {
+     console.error("Errore nell'inizializzazione del database:", err);
    });
